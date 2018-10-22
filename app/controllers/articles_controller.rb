c3bc33e1b4da1d11d15ebbb75b7a991af07eb114
mperaic <mperaic@gmail.com>
2018-10-22 10:46:51 +0200

    add update action to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new 
   end
   
+  def edit 
+    @article = Article.find(params[:id]) 
+  end 
+  
   def create
     @article = Article.new(article_params)
     
@@ -21,6 +25,17 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def update
+    @article = Article.find(params[:id]) 
+    
+    if @article.update(article_params) 
+      redirect_to @article 
+    else
+      render 'edit' 
+    end 
+  end
+    
+    
   private
     def article_params
       params.require(:article).permit(:title, :text)
